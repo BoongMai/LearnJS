a958cfe83b2c60aeb5afe439de56dee225344695
BoongMai <thanhduypham9@gmail.com>
2021-12-01 20:48:38 +0700

    Ngày học JS đầu tiên

---
+++ main.js
@@ -0,0 +1,38 @@
+var fullName = 'Pham Thanh Duy', // khai báo tên cho biến fullName
+    age = 18 // toán tử gán
+
+//---------------------------------   
+// alert(fullName) /** Thông báo ra màn hình biến fullName */
+// alert(age)
+
+// console.log(fullName) // hiển thị thông tin ra consle
+
+// confirm(); // hỏi ý
+// prompt(); // có nhập thông tin
+
+// setTimeout(function() {
+//     alert('Đợi 2s đi')
+// }, 2000);
+
+// setInterval(function() {
+//     console.log('Log chạy' + Math.random())
+// }, 1000);
+//-----------------------------------
+var a = 1 + 2 ; // toán tử số học, trong JS gán ==
+console.log(a)
+
+
+var a = 2;
+var b = 2;
+if(a == b){
+    console.log('Đúng')
+}
+
+// Toán tử logic đấu &&, cả hai vế phải đúng.
+
+//-------------------------
+// Prefix & Postfix = tiền tố, hậu tố
+var number = 4;
+var output = --number + number++;
+
+console.log('Out put: ', output);
