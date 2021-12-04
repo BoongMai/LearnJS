2f625642692a7fb2e12ee431e6464db416e14bce
BoongMai <thanhduypham9@gmail.com>
2021-12-04 17:53:30 +0700

    days 3

--- main.js
+++ main.js
@@ -1,55 +1,73 @@
-var fullName = 'Pham Thanh Duy', // khai báo tên cho biến fullName
-    age = 18 // toán tử gán
+// var fullName = 'Pham Thanh Duy', // khai báo tên cho biến fullName
+//     age = 18 // toán tử gán
 
-//---------------------------------   
-// alert(fullName) /** Thông báo ra màn hình biến fullName */
-// alert(age)
+// //---------------------------------   
+// // alert(fullName) /** Thông báo ra màn hình biến fullName */
+// // alert(age)
 
-// console.log(fullName) // hiển thị thông tin ra consle
+// // console.log(fullName) // hiển thị thông tin ra consle
 
-// confirm(); // hỏi ý
-// prompt(); // có nhập thông tin
+// // confirm(); // hỏi ý
+// // prompt(); // có nhập thông tin
 
-// setTimeout(function() {
-//     alert('Đợi 2s đi')
-// }, 2000);
+// // setTimeout(function() {
+// //     alert('Đợi 2s đi')
+// // }, 2000);
 
-// setInterval(function() {
-//     console.log('Log chạy' + Math.random())
-// }, 1000);
-//-----------------------------------
-var a = 1 + 2 ; // toán tử số học, trong JS gán ==
-console.log(a)
+// // setInterval(function() {
+// //     console.log('Log chạy' + Math.random())
+// // }, 1000);
+// //-----------------------------------
+// var a = 1 + 2 ; // toán tử số học, trong JS gán ==
+// console.log(a)
 
 
-var a = 2;
-var b = 2;
-if(a == b){
-    console.log('Đúng')
-}
+// var a = 2;
+// var b = 2;
+// if(a == b){
+//     console.log('Đúng')
+// }
 
 // Toán tử logic đấu &&, cả hai vế phải đúng.
 
 //-------------------------
 // Prefix & Postfix = tiền tố, hậu tố
-var number = 4;
-var output = --number + number++;
+// var number = 4;
+// var output = --number + number++;
 
-console.log('Out put: ', output);
-console.log('Cun Cat');
-
-
-
-// --------------------------
-
-bài tập prefix & Postfix
-var a;
-var b;
-
-var a = 7;
-var b = 6;
-
-var c = a++ + --a + --b 
+// console.log('Out put: ', output);
+// console.log('Cun Cat');
 
 
-console.log(c)
+
+// // --------------------------
+
+// //bài tập prefix & Postfix
+// var a;
+// var b;
+
+// var a = 7;
+// var b = 6;
+
+// var c = a++ + --a + --b 
+//         7     +  7 +  5
+
+// console.log(c)
+
+// Học toán tử gán
+// var result = '4' + 2 + 1 
+
+// console.log(result)
+/**
+ * Kiểu Boolean có 2 giá trị là true và false
+ */
+var a = 2;
+var b = 2;
+
+if (typeof b == "number" && typeof a == "number"){
+  result = a + b;
+ console.log(result)
+}else{
+    console.log('cc')
+}
+
